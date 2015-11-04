f6724002c5217a72a746322a2faf5413ba11b883
Tasdik Rahman <tasdik95@gmail.com>
2015-11-05 01:25:07 +0530

    added the %, ^2 and exp functions. Up and working.To add undo

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.4
 
 from tkinter import *
+import time
 
 root = Tk()
 root.title('Calculator')
@@ -26,8 +27,9 @@ def get_variables(num):
 
 def get_operation(operator):
     global i
+    length = len(operator)
     display.insert(i, operator)
-    i += 1
+    i += length
 
 def calculate():
     whole_string = display.get()
@@ -57,11 +59,51 @@ def calculate():
         except Exception:
             display.insert(0, "Zero division error!")
 
+    ### functions to be added 
+    elif "%" in whole_string:
+        var1, var2 = whole_string.split("%")
+        clear_all()
+        try:
+            result = int(var1) % int(var2)
+            display.insert(0, result)
+        except Exception:
+            display.insert(0, "Zero division error!")
+    
+    elif "^2" in whole_string:
+        var1, var2 = whole_string.split("^")
+        clear_all()
+        value = int(var1)
+        ## note: the if is not working, figure out an alternative
+        if value < 0:
+            new_value = +value
+            result = new_value*new_value
+        else:
+            result = value * value
+        display.insert(0,result)
+    elif "exp" in whole_string:
+        var1, var2 = whole_string.split("exp")
+        clear_all()
+        result = 1
+        counter = int(var2)
+        while counter > 0:
+            result *= int(var1)
+            counter -= 1
+        display.insert(0, result)
+
+
+def undo():
+    ## removes the last entered operator/variable from entry widget
+    whole_string = display.get()
+    new_string = whole_string[:-1]
+    clear_all()
+    display.insert(0, new_string)
+
 
 root.columnconfigure(0,pad=3)
 root.columnconfigure(1,pad=3)
 root.columnconfigure(2,pad=3)
 root.columnconfigure(3,pad=3)
+root.columnconfigure(4,pad=3)
 
 root.rowconfigure(0,pad=3)
 root.rowconfigure(1,pad=3)
@@ -69,7 +111,7 @@ root.rowconfigure(2,pad=3)
 root.rowconfigure(3,pad=3)
 
 display = Entry(root, font = ("Calibri", 13))
-display.grid(row = 1, columnspan = 4, sticky = W+E)
+display.grid(row = 1, columnspan = 5, sticky = W+E)
 
 one = Button(root, text = "1", command = lambda : get_variables(1), font=("Calibri", 12))
 one.grid(row = 2, column = 0)
@@ -92,7 +134,7 @@ eight.grid(row = 4, column = 1)
 nine = Button(root , text = "9", command = lambda : get_variables(9), font=("Calibri", 12))
 nine.grid(row = 4, column = 2)
 
-cls = Button(root, text = "cls", command = clear_all, font=("Calibri", 12))
+cls = Button(root, text = "C", command = clear_all, font=("Calibri", 12), foreground = "red")
 cls.grid(row = 5, column = 0)
 zero = Button(root, text = "0", command = lambda : get_variables(0), font=("Calibri", 12))
 zero.grid(row = 5, column = 1)
@@ -107,5 +149,16 @@ multiply = Button(root,text = "*", command =  lambda : get_operation("*"), font=
 multiply.grid(row = 4, column = 3)
 divide = Button(root, text = "/", command = lambda :  get_operation("/"), font=("Calibri", 12))
 divide.grid(row = 5, column = 3)
+
+## adding new ones
+# undo = Button(root, text = "<-", command = lambda : undo, font =("Calibri", 12), foreground = "red")
+# undo.grid(row = 2, column = 4)
+modulo = Button(root, text = "%", command = lambda :  get_operation("%"), font=("Calibri", 12))
+modulo.grid(row = 3, column = 4)
+square = Button(root, text = "^2", command = lambda: get_operation("^2"), font = ("Calibri", 10))
+square.grid(row = 4, column = 4)
+exp = Button(root, text = "exp", command = lambda: get_operation("exp"), font = ("Calibri", 10))
+exp.grid(row = 5, column = 4)
+
 
 root.mainloop()
